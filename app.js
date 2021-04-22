fa59678e1907b178af5e8cd0cdaa112abc9d831d
Peter Irlam <irlampeter@gmail.com>
2021-04-22 15:47:57 +0100

    Create ordinal suffix of function

--- app.js
+++ app.js
@@ -48,3 +48,18 @@ function doGet(e) {
     //return HtmlService.createHtmlOutputFromFile('Error');
   }
 }
+
+function ordinal_suffix_of(i) {
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return "st";
+  }
+  if (j == 2 && k != 12) {
+    return "nd";
+  }
+  if (j == 3 && k != 13) {
+    return "rd";
+  }
+  return "th";
+}
