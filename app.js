8a00463795f759c23da00163aad8ceac85fce33b
Peter Irlam <irlampeter@gmail.com>
2022-07-21 10:32:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
 function doPost(e) {
   if (typeof e !== 'undefined')
-    var role = e.parameter.role;
-  var workplace = e.parameter.workplace;
-  var team = e.parameter.team;
-  var Fname = e.parameter.Fname_001.replace(/^\s+|\s+$/g, '');
-  var Lname = e.parameter.Lname_001.replace(/^\s+|\s+$/g, '');
-  var email = e.parameter.email_001.replace(/^\s+|\s+$/g, '');
-  var phone = e.parameter.phone_001.replace(/^\s+|\s+$/g, '');
-  var leavingdate = e.parameter.leavingdate;
-  var startingdate = e.parameter.startingdate;
-  var addrline1 = e.parameter.addrline1_001.replace(/^\s+|\s+$/g, '');
-  var addrline2 = e.parameter.addrline2_001.replace(/^\s+|\s+$/g, '');
-  var town = e.parameter.town_001.replace(/^\s+|\s+$/g, '');
-  var county = e.parameter.county_001.replace(/^\s+|\s+$/g, '');
-  var postcode = e.parameter.postcode_001.replace(/^\s+|\s+$/g, '');
-  var reasonforleaving = e.parameter.reasonforleaving;
-  var outstandingexpenses = e.parameter.outstandingexpenses;
-  var charityequipment = e.parameter.charityequipment;
-  var days_in_post = e.parameter.days_in_post;
+  let role = e.parameter.role;
+  let workplace = e.parameter.workplace;
+  let team = e.parameter.team;
+  let Fname = e.parameter.Fname_001.replace(/^\s+|\s+$/g, '');
+  let Lname = e.parameter.Lname_001.replace(/^\s+|\s+$/g, '');
+  let email = e.parameter.email_001.replace(/^\s+|\s+$/g, '');
+  let phone = e.parameter.phone_001.replace(/^\s+|\s+$/g, '');
+  let leavingdate = e.parameter.leavingdate;
+  let startingdate = e.parameter.startingdate;
+  let addrline1 = e.parameter.addrline1_001.replace(/^\s+|\s+$/g, '');
+  let addrline2 = e.parameter.addrline2_001.replace(/^\s+|\s+$/g, '');
+  let town = e.parameter.town_001.replace(/^\s+|\s+$/g, '');
+  let county = e.parameter.county_001.replace(/^\s+|\s+$/g, '');
+  let postcode = e.parameter.postcode_001.replace(/^\s+|\s+$/g, '');
+  let reasonforleaving = e.parameter.reasonforleaving;
+  let outstandingexpenses = e.parameter.outstandingexpenses;
+  let charityequipment = e.parameter.charityequipment;
+  let days_in_post = e.parameter.days_in_post;
 
   sendLetter(role, workplace, team, Fname, Lname, email, phone, startingdate, leavingdate, addrline1, addrline2, town, county, postcode, reasonforleaving, outstandingexpenses, charityequipment, days_in_post);
-  var html = HtmlService.createTemplateFromFile('Thankyou');
+  let html = HtmlService.createTemplateFromFile('Thankyou');
   return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 /*  Get the URL for the Google Apps Script running as a WebApp. */
 function getScriptUrl() {
-  var url = ScriptApp.getService().getUrl();
+  let url = ScriptApp.getService().getUrl();
   return url;
 }
 
 function doGet(e) {
-  var currentuser = Session.getActiveUser().getEmail();
-  var authusers = ["sboocock@calancs.org.uk", "aoshea@calancs.org.uk", "gsimpson@calancs.org.uk", "mdeslandes@calancs.org.uk", "myates@calancs.org.uk",
+  let currentuser = Session.getActiveUser().getEmail();
+  let authusers = ["sboocock@calancs.org.uk", "aoshea@calancs.org.uk", "gsimpson@calancs.org.uk", "mdeslandes@calancs.org.uk", "myates@calancs.org.uk",
     "jleadbetter@calancs.org.uk", "icook@calancs.org.u", "ccatterall@calancs.org.uk", "mdeslandes@calancs.org.uk",
     "sbennett@calancs.org.uk", "admin@calancs.org.uk", "gsimpson@calancs.org.uk", "mdeslandes@calancs.org.uk",
     "pfurner@calancs.org.uk", "amilligan@calancs.org.uk", "nhigham@calancs.org.uk", "swalsh@calancs.org.uk", "jasbury@calancs.org.uk", "pirlam@calancs.org.uk"
   ];
-  var isauthorised = authusers.indexOf(currentuser);
+  let isauthorised = authusers.indexOf(currentuser);
   if (isauthorised >= 0) {
     return HtmlService.createHtmlOutputFromFile('Form');
   } else {
-    var html = HtmlService.createTemplateFromFile('Error');
+    let html = HtmlService.createTemplateFromFile('Error');
     html.currentuser = currentuser;
     return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
       .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
@@ -50,7 +50,7 @@ function doGet(e) {
 }
 
 function ordinal_suffix_of(i) {
-  var j = i % 10,
+  let j = i % 10,
     k = i % 100;
   if (j == 1 && k != 11) {
     return "st";
@@ -65,7 +65,7 @@ function ordinal_suffix_of(i) {
 }
 
 function sendLetter(role, workplace, team, Fname, Lname, email, phone, startingdate, leavingdate, addrline1, addrline2, town, county, postcode, reasonforleaving, outstandingexpenses, charityequipment, days_in_post) {
-  var roletype;
+  let roletype;
   switch (role) {
     case "Volunteer":
       roletype = "volunteering role";
@@ -75,14 +75,14 @@ function sendLetter(role, workplace, team, Fname, Lname, email, phone, startingd
       break;
   }
   //Set up destination folder
-  var dstFolderId = DriveApp.getFolderById("1lVsjyYWp2VdvSqEF4lgfgMXF6kEK-FcN");
+  let dstFolderId = DriveApp.getFolderById("1lVsjyYWp2VdvSqEF4lgfgMXF6kEK-FcN");
   //Convert date to ISO format (yyyy/mm/dd)
-  var startdateArray = startingdate.split("/");
-  var leavingdateArray = leavingdate.split("/");
-  var startdate = new Date(startdateArray[2] + "/" + startdateArray[1] + "/" + startdateArray[0]);
-  var leavingdate = new Date(leavingdateArray[2] + "/" + leavingdateArray[1] + "/" + leavingdateArray[0]);
-  var ordinal;
-  var day;
+  let startdateArray = startingdate.split("/");
+  let leavingdateArray = leavingdate.split("/");
+  let startdate = new Date(startdateArray[2] + "/" + startdateArray[1] + "/" + startdateArray[0]);
+  let leavingdate = new Date(leavingdateArray[2] + "/" + leavingdateArray[1] + "/" + leavingdateArray[0]);
+  let ordinal;
+  let day;
   if (leavingdateArray[0].substring(0, 1) == "0") {
     ordinal = ordinal_suffix_of(leavingdateArray[0].substring(1))
     day = leavingdateArray[0].substring(1);
@@ -90,7 +90,7 @@ function sendLetter(role, workplace, team, Fname, Lname, email, phone, startingd
     ordinal = ordinal_suffix_of(leavingdateArray[0])
     day = leavingdateArray[0];
   }
-  var monthname;
+  let monthname;
   switch (leavingdateArray[1]) {
     case "01":
       monthname = "January";
@@ -129,9 +129,9 @@ function sendLetter(role, workplace, team, Fname, Lname, email, phone, startingd
       monthname = "December";
       break;
   }
-  var leavingdateLongFormat = day + ordinal + " " + monthname + " " + leavingdateArray[2];
+  let leavingdateLongFormat = day + ordinal + " " + monthname + " " + leavingdateArray[2];
   //Set up address
-  var address;
+  let address;
   if (addrline2.length > 0) {
     if (county.length > 1) {
       address = addrline1 + '\r' + addrline2 + '\r' + town + '\r' + county + '\r' + postcode;
@@ -147,9 +147,9 @@ function sendLetter(role, workplace, team, Fname, Lname, email, phone, startingd
   }
   //Leavers Letter
   if (days_in_post >= 180) {
-    var docid = DriveApp.getFileById("1n3zWEttQA8LiwADEBFH-a5oHzU6ycGBhz0KM3l_d2fs").makeCopy("Leavers_Letter_" + Utilities.formatDate(new Date(), "GMT+1", "dd-MMM-yyyy") + "_" + name, dstFolderId).getId()
-    var doc = DocumentApp.openById(docid);
-    var body = doc.getActiveSection();
+    let docid = DriveApp.getFileById("1n3zWEttQA8LiwADEBFH-a5oHzU6ycGBhz0KM3l_d2fs").makeCopy("Leavers_Letter_" + Utilities.formatDate(new Date(), "GMT+1", "dd-MMM-yyyy") + "_" + name, dstFolderId).getId()
+    let doc = DocumentApp.openById(docid);
+    let body = doc.getActiveSection();
     body.replaceText("%fullname%", Fname + " " + Lname);
     body.replaceText("%addrline1%", address);
     body.replaceText("%town%", town);
@@ -169,8 +169,8 @@ function sendLetter(role, workplace, team, Fname, Lname, email, phone, startingd
     });
   }
   //Leavers Report
-  var docid2 = DriveApp.getFileById("1dm7XshpRE1jsHhUkV45X61-eYA1IjW6ut2H6g1mxhzE").makeCopy("Leavers_Report_" + Utilities.formatDate(new Date(), "GMT+1", "dd-MMM-yyyy") + "_" + name, dstFolderId).getId();
-  var doc2 = DocumentApp.openById(docid2);
+  let docid2 = DriveApp.getFileById("1dm7XshpRE1jsHhUkV45X61-eYA1IjW6ut2H6g1mxhzE").makeCopy("Leavers_Report_" + Utilities.formatDate(new Date(), "GMT+1", "dd-MMM-yyyy") + "_" + name, dstFolderId).getId();
+  let doc2 = DocumentApp.openById(docid2);
   if (phone.length < 1) {
     phone = "Not recorded";
   }
@@ -183,7 +183,7 @@ function sendLetter(role, workplace, team, Fname, Lname, email, phone, startingd
   if (charityequipment.length < 1) {
     charityequipment = "None stated";
   }
-  var body2 = doc2.getActiveSection();
+  let body2 = doc2.getActiveSection();
   body2.replaceText("%fullname%", Fname + " " + Lname);
   body2.replaceText("%addrline1%", address)
   body2.replaceText("%town%", town);
